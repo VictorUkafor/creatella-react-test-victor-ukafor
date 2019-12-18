09afce25b90c6fab450643348747bd9d6e9bfe6c
Victor Ukafor <victorukafor@gmail.com>
2019-12-18 03:21:38 +0100

    add styling header design

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,42 @@
+
+// header component
 const Header = () => {
     return (
-    <header>
-        <h1>Products Grid</h1>
+    <header className="header">
+        <div>
 
-        <p>Here you're sure to find a bargain on some of the finest
+            {/* logo */}
+            <h1 className="logo">
+                <span className="first">Products</span>
+                <span className="second">Grid</span>
+            </h1>
+            
+            {/* select form for sorting products */}
+            <form className="sort-form">
+                <div className="form-row align-items-center">
+                    <div className="col-auto my-1 select">
+                        <select className="custom-select mr-sm-2 sort-select">
+                            <option selected>Sort by </option>
+                            <option value="size">Size</option>
+                            <option value="price">Price</option>
+                            <option value="id">ID</option>
+                        </select>
+                    </div>
+                   <div className="">
+                        <button type="button" className="btn go-button">Go</button>
+                   </div>
+                </div>
+            </form>
+
+        </div>
+
+        <hr className="header-line"/>
+
+        {/* intro on the header */}
+        <p className="header-intro">Here you're sure to find a bargain on some of the finest
          ascii available to purchase. Be sure to peruse our selection
-          of ascii faces in an exciting range of sizes and prices.</p>
+          of ascii faces in an exciting range of sizes and prices.
+        </p>
     </header>
     ) ;
 }
@@ -28,5 +59,4 @@ const Body = () => {
     );
 }
 
-const body = document.querySelector('body');
-ReactDOM.render(<Body />, body);
+ReactDOM.render(<Body />, document.querySelector('body'));
